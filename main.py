5417988edf7c67adaa2f2b2beabf1ec6e0f0063b
Rafal Ziewiecki <ziewiecki.rafal@gmail.com>
2022-06-17 13:15:20 +0200

    kolejna część

--- main.py
+++ main.py
@@ -1,22 +1,53 @@
+from re import T
+
+
 def start():    
     print("Witaj w programie który rozwiąrze każde zadanie fizyczne z jedną niewiadomą.")
+    print("Wpisuj dane według układu jednostek miar SI")
 start()
 
 def dane():
-    print("\nNajpierw poproszę cię o wpisanie danych. Wprowadź 'x' gdzie nie posiadasz danych.")
-    s=input("droga = ")
-    t=input("czas = ")
-    v=input("prędkość = ")
-    if s == "x":
-        print("\nZatem droga jest naszą niewiadomą.")
-    elif t == "x":
-        print("\nZatem czas jest naszą niewiadomą.")
-    elif v == "x":
-        print("\nZatem prędkość jest naszą niewiadomą.")
+    print("\nNajpierw poproszę cię o wpisanie danych. Wprowadź odpowiedni numer który odpowiada nie posiadania danych podanej rzeczy.")
+    print("\n1. Droga\n2. Czas\n3. Prędkość")
+    x = int(input("\n"))
+    if x == 1:
+        print("\nWprowadź resztę danych:")
+        t = int(input("czas = "))
+        v = int(input("prędkość = "))
+        s = "x"
+    elif x == 2:
+        print("\nWprowadź resztę danych:")
+        s = int(input("droga = "))
+        v = int(input("prędkość = "))
+        t = "x"
+    elif x == 3:
+        print("\nWprowadź resztę danych:")
+        t = int(input("czas = "))
+        v = "x"
+        s = int(input("droga = "))
     print("\n\nPodsumujmy:\ndroga =",s ,"\nczas = ",t ,"\nprędkość =",v)
     obliczenia(s, t, v)
 def obliczenia(a, b, c):
-    print("Przechodzimy do następnej części zadania czyli do obliczeń.")
+    wzor = "V=S/t"
+    print("\n\nPrzechodzimy do następnej części zadania czyli do obliczeń.")
+    print("\nWzór, który tu użyjemy to:", wzor)
+    if c == "x":
+        print("Oblicznenia:", c, "=", a, "/", b)
+        print("\n", c,"=", a/b)
+        print("Wynik końcowy:", a/b, "m/s")
+        print("\t",(a/1000)/(b/3600), "km/h")
+    if a == "x":
+        print("Oblicznenia:", c, "=", a, "/", b)
+        print("Przekształcanie wzoru:", a, "=", c, "*", b)
+        print("\n", a,"=", c*b)
+        print("Wynik końcowy:", c*b, "m")
+        print("\t", c*b/1000, "km")
+    if b == "x":
+        print("Oblicznenia:", c, "=", a, "/", b)
+        print("Przekształcenie wzoru:", b, "=", a, "/", c)
+        print("\n", b,"=", a/c)
+        print("Wynik końcowy:", a/c, "s")
+        print("\t", a/c/60, "h")
 def exercise():
     dane()
 exercise()
